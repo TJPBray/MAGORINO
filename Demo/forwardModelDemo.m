a85b9c5015cd900e527ca9b48214c7a2e6e9cb8c
TJPBray <t.bray@ucl.ac.uk>
2021-09-30 12:05:05 +0100

    Update demo script

--- Demo/forwardModelDemo.m
+++ Demo/forwardModelDemo.m
@@ -3,29 +3,37 @@
 %% 1. Generate the noise-free forward model
 
 % 1.1 set the tissue parameters
+% [Sf(au),  Sw(au), R2*(kHz), fB(kHz)]
 
-% R2* (kHz or ms^-1)
-R2star = 0.1;
+%Define S0
+    S0=100;
 
-% field strength offset (kHz)
-fieldStrengthOffset = 0.0;
+    %1. Zero FF, low R2*
+    params(1,:) =    [0, S0, 0, 0];
+    %2. Low FF, low R2*
+    params(2,:  )=   [0.2*S0, 0.8*S0, 0, 0];
+    %3. Middle FF, low R2*
+    params(3,:)  =   [0.6*S0, 0.4*S0, 0, 0];
+    %4. High FF, low R2*
+    params(4,:)  =   [0.9*S0, 0.1*S0, 0, 0];
+    %5. Zero FF, high R2*
+    params(5,:)  =   [0*S0, 1*S0, 0.5, 0];
+    %6. Low FF, high R2*
+    params(6,:)  =   [0.2*S0, 0.8*S0, 0.5, 0];
+    %7. Middle FF, high R2*
+    params(7,:)  =   [0.5*S0, 0.5*S0, 0.5, 0];
+    %8. High FF, high R2*
+    params(8,:)   =  [0.9*S0, 0.1*S0, 0.5, 0];
 
-% S0 (arbitrary unit) - corresponding to the theoretical signal at TE = 0
-S0 = 1;
-
-% fat fraction
-fatFraction = 0.2;
-
-% convert S0 and fat fraction to fat and water signals
-
-% fat signal (arbitrary unit)
-Sfat = S0*fatFraction;
-
-% water signal (arbitrary unit)
-Swater = S0*(1 - fatFraction);
-
-% combine into ground truth parameter vector
-GT = [Sfat; Swater; R2star];
+    %Pick tissue type
+    tissue=2;
+    
+    %Define parameter vector
+    p=params(tissue,:)';
+    
+    %Add ground truth parameters to GT structure to provide to fitting
+    GT.p=p;
+    
 
 % 1.2 set the acquisition parameters
 
@@ -33,29 +41,40 @@ GT = [Sfat; Swater; R2star];
 fieldStrength = 3;
 
 % echo times (ms) - densely sampled here for visualisation purposes
-echoTimes = (0:0.1:10)';
+denseEchoTimes = (0:0.1:11)';
+
+%Sampling scheme
+sampling=[12:11:111];
+
+% Sampled echo times
+echoTimes = denseEchoTimes(sampling);
 
 % number of measurements
 numOfMeas = length(echoTimes);
 
-% 1.3 generate the signal with Fatfunction
-signalNoiseFree = MultiPeakFatSingleR2(echoTimes, fieldStrength, Sfat, Swater, R2star, fieldStrengthOffset);
+% 1.3 generate the noise-free measured signal with Fatfunction
+signalNoiseFreeDense = MultiPeakFatSingleR2(denseEchoTimes, fieldStrength, p(1), p(2), p(3), p(4));
+
+signalNoiseFree = MultiPeakFatSingleR2(echoTimes, fieldStrength, p(1), p(2), p(3), p(4));
+
+% add noise-free signal to ground truth structure 
+GT.S=signalNoiseFree;
 
 % 1.4 visualise the signal
 figure;
 h1 = subplot(2, 2, 1);
 hold on;
-plot(echoTimes, real(signalNoiseFree), 'r-');
-plot(echoTimes, imag(signalNoiseFree), 'b-');
+plot(denseEchoTimes, real(signalNoiseFreeDense), 'r-');
+plot(denseEchoTimes, imag(signalNoiseFreeDense), 'b-');
 xlabel('echo time (ms)');
 ylabel('signal');
 legend('noise-free real', 'noise-free imag');
 
 % 1.5 convert to magnitude and phase
-signalMagnNoiseFree = abs(signalNoiseFree);
+signalMagnNoiseFree = abs(signalNoiseFreeDense);
 h3 = subplot(2, 2, 3);
 hold on;
-plot(echoTimes, signalMagnNoiseFree, 'r-');
+plot(denseEchoTimes, signalMagnNoiseFree, 'r-');
 xlabel('echo time (ms)');
 ylabel('signal');
 legend('noise-free magnitude');
@@ -65,12 +84,11 @@ signalPhaseNoiseFree = angle(signalNoiseFree);
 %% 2. Simulate noisy measured signal from noise free data
 
 % 2.1 fix the random seed
-rng(1);
 
 % 2.2 set the standard deviation of the Gaussian noise
 
 % set the SNR (SNR = S0/sigma)
-SNR = 10;
+SNR = 40;
 
 % set the sigma
 sigma = S0/SNR;
@@ -114,9 +132,39 @@ xlabel('noise (rotated)');
 ylabel('count');
 legend('real', 'imag');
 
+
+
+%% Visualise likelihood for each parameter
+
+
 %% 3. Fit the model to the data
 
 estimatedParas = R2fitting(echoTimes, fieldStrength, signalNoisy, sigma, GT);
 
+%% Visualise predicted signal relative to ground truth and noisy measured signal 
+figure
+subplot(2,2,1)
 
+hold on
 
+%Noise-free signal
+plot(denseEchoTimes, abs(signalNoiseFreeDense), 'k-','Linewidth',2);
+
+%Noisy sampled signal
+plot(echoTimes, abs(signalNoisy), 'k.','MarkerSize',20,'Linewidth',4);
+
+%Gaussian fitted signal
+gaussianPredictedSignal = MultiPeakFatSingleR2(denseEchoTimes, fieldStrength, estimatedParas.standard.F, estimatedParas.standard.W, estimatedParas.standard.R2, 0);
+plot(denseEchoTimes, abs(gaussianPredictedSignal), 'r--','Linewidth',2)
+
+%Rician fitted signal
+ricianPredictedSignal = MultiPeakFatSingleR2(denseEchoTimes, fieldStrength, estimatedParas.Rician.F, estimatedParas.Rician.W, estimatedParas.Rician.R2, 0);
+plot(denseEchoTimes, abs(ricianPredictedSignal), 'b--','Linewidth',2)
+
+%Complex fitted signal
+complexPredictedSignal = MultiPeakFatSingleR2(denseEchoTimes, fieldStrength, estimatedParas.complex.F, estimatedParas.complex.W, estimatedParas.complex.R2, 0);
+plot(denseEchoTimes, abs(complexPredictedSignal), 'g--','Linewidth',2)
+
+hold off 
+
+legend('Noise-free signal','Noisy sampled signal', 'Gaussian fit','Rician fit', 'Complex fit')
