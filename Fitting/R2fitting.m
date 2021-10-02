1d6fe668900a458465f91760aabf81d601cd4542
TJPBray <t.bray@ucl.ac.uk>
2021-10-02 23:13:45 +0100

    Reorganise so that fitting is performed in separate functions

--- Fitting/R2fitting.m
+++ Fitting/R2fitting.m
@@ -23,272 +23,65 @@ Smagnitude=abs(Smeasured); %otherwise use magnitude
 
 %Set initialisation value for R2*: vinit
 vinit=0.1;
+algoparams.vinit=0.1;
 vmax=1;
 vmin=0; %negative value for min to avoid penalisation at boundary
 
 %Set signal initialisation for fat and water: Sinit
 C=exp(vinit);
 Sinit=C*max(Smagnitude); %partially compensates for R2* to improve initialisation
+algoparams.Sinit=Sinit;
 % Sinit=100;
 
-%% Set up the optimisation framework for standard magnitude fitting
+%% Set optimisation options
+algoparams.options=optimoptions('fmincon', 'Algorithm', 'interior-point','InitBarrierParam',100000,'ScaleProblem',true,'FiniteDifferenceType','central');
+algoparams.solver = 'fmincon';
 
-% define the objective function
-R2fitting.objective = @(p) -R2Obj(p,echotimes,tesla,Smagnitude,sig);
-
-% set the solver 
-R2fitting.solver = 'fmincon';
-
-% use interior-point
-R2fitting.options = optimoptions('fmincon', 'Algorithm', 'interior-point','InitBarrierParam',100000,'ScaleProblem',true,'FiniteDifferenceType','central');
-%Barrier parameter might reduce step size and avoid bypassing of correct
-%minimum; can include 'InitBarrierParam',100, in options
-%'FiniteDifferenceType','central' might improve accuracy of derivative
-%estimation
+%% Gaussian ('standard') magnitude fitting
 
 % set the parameter lower bound
-R2fitting.lb = [0, 0, vmin]'; 
+algoparams.lb = [0, 0, vmin]';
 
 % % set the parameter upper bound
-R2fitting.ub = [3*Sinit, 3*Sinit, vmax]'; %constrain fB0 to 0 for now
+algoparams.ub = [3*Sinit, 3*Sinit, vmax]'; 
 
+% Call GaussianMagnitudeFitting
+outparams.standard = GaussianMagnitudeFitting (echotimes, tesla, Scomplex, sig, GT, algoparams);
 
-%% Implement standard magnitude fitting for both water-dominant and fat-dominant initialisations
-% [F W R2* fB0]
+%% Gaussian ('standard') magnitude fitting
 
-% First assume LOW FF (WATER DOMINANT) TISSUE (Use first echo to provide water guess)
-R2fitting.x0 = [0.001, Sinit, vinit]'; 
-
-% run the optimisation
-[pmin1_mag, fmin1_mag] = fmincon(R2fitting); %fmin is the minimised SSE
-
-% Next assume HIGH FF (FAT DOMINANT) TISSUE (Use first echo to provide water guess)
-R2fitting.x0 = [Sinit, 0.001, vinit]'; 
-
-% run the optimisation
-[pmin2_mag, fmin2_mag] = fmincon(R2fitting); %fmin is the minimised SSE
-
-% Next INITIALISE WITH GROUND TRUTH
-R2fitting.x0 = GT.p(1:3); %Only three params for magnitude fitting
-
-% run the optimisation
-[pmin3_mag, fmin3_mag] = fmincon(R2fitting); %fmin is the minimised SSE
-
-%Add the solutions to outparams
-outparams.standard.pmin1=pmin1_mag;
-outparams.standard.pmin2=pmin2_mag;
-outparams.standard.pmin3=pmin3_mag;
-
-outparams.standard.fmin1=fmin1_mag;
-outparams.standard.fmin2=fmin2_mag;
-outparams.standard.fmin3=fmin3_mag;
-
-% Choose the estimates from the best residual and add those to the
-% outparams structure
-if fmin1_mag<=fmin2_mag
-    
-outparams.standard.F = pmin1_mag(1);
-outparams.standard.W = pmin1_mag(2);
-outparams.standard.R2 = pmin1_mag(3);
-
-outparams.standard.fmin = fmin1_mag;
-outparams.standard.chosenmin=1;
-
-% Calculate SSE for standard (Gaussian) fitting (NB this is different to fmin above, which corresponds to likelihood)
-[~,outparams.standard.SSE]=R2Obj(pmin1_mag,echotimes,tesla,Smagnitude,sig);
-
-% Calculate SSE for standard (Gaussian) fitting relative to noise-free
-% signal
-[~,outparams.standard.SSEtrue]=R2Obj(pmin1_mag,echotimes,tesla,GT.S,sig);
-
-else
-    
-outparams.standard.F = pmin2_mag(1); %swap fat and water from lines above
-outparams.standard.W = pmin2_mag(2);
-outparams.standard.R2 = pmin2_mag(3);
-
-outparams.standard.fmin = fmin2_mag;
-outparams.standard.chosenmin=2;
-
-% Calculate SSE for standard (Gaussian) fitting (NB this is different to fmin above, which corresponds to likelihood)
-[~,outparams.standard.SSE]=R2Obj(pmin2_mag,echotimes,tesla,Smagnitude,sig);
-
-% Calculate SSE for standard (Gaussian) fitting relative to noise-free
-% signal
-[~,outparams.standard.SSEtrue]=R2Obj(pmin2_mag,echotimes,tesla,GT.S,sig);
-
-end
-
-% Calculate SSE for standard (Gaussian) fitting for ground-truth
-% initialised value
-[~,outparams.standard.SSEgtinit]=R2Obj(pmin3_mag,echotimes,tesla,Smagnitude,sig);
-[~,outparams.standard.SSEtrue_gtinit]=R2Obj(pmin3_mag,echotimes,tesla,abs(GT.S),sig);
-
-%% Set up the optimisation framework for  magnitude fitting with Rician noise modelling
-
-% Replicate settings from above
-R2Ricianfitting.solver=R2fitting.solver;
-R2Ricianfitting.options=R2fitting.options;
-R2Ricianfitting.lb=R2fitting.lb;
-R2Ricianfitting.ub=R2fitting.ub;
-R2Ricianfitting.x0=R2fitting.x0;
-
-% define the objective function
-R2Ricianfitting.objective = @(p) -R2RicianObj(p,echotimes,tesla,Smagnitude,sig); 
-
-%% Implement Rician fitting for both water-dominant and fat-dominant initialisations
-% [F W R2* fB0]
-
-% First assume LOW FF (WATER DOMINANT) TISSUE (Use first echo to provide water guess)
-R2Ricianfitting.x0 = [0.001, Sinit, vinit]'; 
-
-% run the optimisation
-[pmin1_Ric, fmin1_Ric] = fmincon(R2Ricianfitting); %fmin is the minimised SSE
-
-% Next assume HIGH FF (FAT DOMINANT) TISSUE (Use first echo to provide water guess)
-R2Ricianfitting.x0 = [Sinit, 0.001, vinit]'; 
-
-% run the optimisation
-[pmin2_Ric, fmin2_Ric] = fmincon(R2Ricianfitting); %fmin is the minimised SSE
-
-% Next INITIALISE WITH GROUND TRUTH
-R2Ricianfitting.x0 = GT.p(1:3); %Only three params for magnitude fitting
-
-% run the optimisation
-[pmin3_Ric, fmin3_Ric] = fmincon(R2Ricianfitting); %fmin is the minimised SSE
-
-%Add the  solutions to outparams
-outparams.Rician.pmin1=pmin1_Ric;
-outparams.Rician.pmin2=pmin2_Ric;
-outparams.Rician.pmin3=pmin3_Ric;
-
-outparams.Rician.fmin1=fmin1_Ric;
-outparams.Rician.fmin2=fmin2_Ric;
-outparams.Rician.fmin3=fmin3_Ric;
-
-% Choose the estimates from the best residual and add to outparams
-
-if fmin1_Ric<=fmin2_Ric
-    
-outparams.Rician.F = pmin1_Ric(1);
-outparams.Rician.W = pmin1_Ric(2);
-outparams.Rician.R2 = pmin1_Ric(3);
-
-outparams.Rician.fmin = fmin1_Ric; %For Rician fitting, fmin corresponds to the maximum likelihood (i.e. lowest value of -fmin)
-outparams.Rician.chosenmin=1;
-
-% Calculate SSE for Rician fitting (NB this is different to fmin above, which corresponds to likelihood)
-[~,outparams.Rician.SSE]=R2Obj(pmin1_Ric,echotimes,tesla,Smagnitude,sig); %Can use R2Obj here to calculate SSE rather than R2RicianObj
-
-% Calculate SSE for Rician fitting relative to ground truth
-[~,outparams.Rician.SSEtrue]=R2Obj(pmin1_Ric,echotimes,tesla,GT.S,sig); %Can use R2Obj here to calculate SSE rather than R2RicianObj
-
-else
-    
-outparams.Rician.F = pmin2_Ric(1);
-outparams.Rician.W = pmin2_Ric(2);
-outparams.Rician.R2 = pmin2_Ric(3);
-
-outparams.Rician.fmin = fmin2_Ric; %For Rician fitting, fmin corresponds to the maximum likelihood (i.e. lowest value of -fmin)
-outparams.Rician.chosenmin=2;
-
-% Calculate SSE for Rician fitting (NB this is different to fmin above, which corresponds to likelihood)
-[~,outparams.Rician.SSE]=R2Obj(pmin2_Ric,echotimes,tesla,Smagnitude,sig); %Can use R2Obj here to calculate SSE rather than R2RicianObj
-
-% Calculate SSE for Rician fitting relative to ground truth
-[~,outparams.Rician.SSEtrue]=R2Obj(pmin2_Ric,echotimes,tesla,GT.S,sig); %Can use R2Obj here to calculate SSE rather than R2RicianObj
-
-end
-
-% Calculate SSE for standard (Gaussian) for ground-truth initialised value
-[~,outparams.Rician.SSEgtinit]=R2Obj(pmin3_Ric,echotimes,tesla,Smagnitude,sig); %For measured signal
-[~,outparams.Rician.SSEtrue_gtinit]=R2Obj(pmin3_Ric,echotimes,tesla,abs(GT.S),sig); %For ground-truth initialised signal
-
-%% Set up the optimisation framework for  complex fitting
-
-% Replicate settings from above
-R2complexfitting.solver=R2fitting.solver;
-R2complexfitting.options=R2fitting.options;
-
-% Define the objective function
-R2complexfitting.objective = @(p) -R2ComplexObj(p,echotimes,tesla,Scomplex,sig);
-
-%% Implement complex fitting for both water-dominant and fat-dominant initialisations
-% [F W R2* fB0]
-
-% First assume LOW FF (WATER DOMINANT) TISSUE (Use first echo to provide water guess)
-R2complexfitting.x0 = [0.001, Sinit, vinit, 0]'; 
-
-%allow fB0 to vary:
 % set the parameter lower bound
-R2complexfitting.lb = [0, 0, vmin, -Inf]';
+algoparams.lb = [0, 0, vmin]';
 
 % % set the parameter upper bound
-R2complexfitting.ub = [3*Sinit, 3*Sinit, vmax, Inf]'; 
+algoparams.ub = [3*Sinit, 3*Sinit, vmax]'; 
 
-% run the optimisation
-[pmin1, fmin1] = fmincon(R2complexfitting); %fmin is the minimised SSE
+% Call RicianMagnitudeFitting
+outparams.Rician = RicianMagnitudeFitting (echotimes, tesla, Scomplex, sig, GT, algoparams);
 
-% Next assume HIGH FF (FAT DOMINANT) TISSUE (Use first echo to provide water guess)
-R2complexfitting.x0 = [Sinit, 0.001, vinit, 0]'; 
+%% Complex fitting
 
-% run the optimisation
-[pmin2, fmin2] = fmincon(R2complexfitting); %fmin is the minimised SSE
+% 1. Perform with variable fB
 
-% Next INITIALISE WITH GROUND TRUTH
-R2complexfitting.x0 = GT.p; %Use all four params for complex fitting
+% set the parameter lower bound
+algoparams.lb = [0, 0, vmin, -Inf]';
 
-% run the optimisation
-[pmin3, fmin3] = fmincon(R2complexfitting); %fmin is the minimised SSE
+% % set the parameter upper bound
+algoparams.ub = [3*Sinit, 3*Sinit, vmax, Inf]'; 
 
+% Call ComplexFitting
+outparams.complex = ComplexFitting (echotimes, tesla, Scomplex, sig, GT, algoparams);
 
-%Add the solutions to outparams
-outparams.complex.pmin1=pmin1;
-outparams.complex.pmin2=pmin2;
-outparams.complex.pmin3=pmin3;
+% 2. Perform with fixed fB
 
-outparams.complex.fmin1=fmin1;
-outparams.complex.fmin2=fmin2;
-outparams.complex.fmin3=fmin3;
+% set the parameter lower bound
+algoparams.lb = [0, 0, vmin, 0]';
 
-% Choose the estimates from the best residual
+% % set the parameter upper bound
+algoparams.ub = [3*Sinit, 3*Sinit, vmax, 0]'; 
 
-if fmin1<=fmin2
-    
-outparams.complex.F = pmin1(1);
-outparams.complex.W = pmin1(2);
-outparams.complex.R2 = pmin1(3);
-
-outparams.complex.fmin = fmin1;
-outparams.complex.chosenmin=1;
-
-% Calculate SSE for complex fitting
-[~,outparams.complex.SSE]=R2ComplexObj(pmin1,echotimes,tesla,Scomplex,sig);
-
-% Calculate SSE for complex fitting relative to noise-free signal
-[~,outparams.complex.SSEtrue]=R2ComplexObj(pmin1,echotimes,tesla,GT.S,sig);
-
-else
-    
-outparams.complex.F = pmin2(1);
-outparams.complex.W = pmin2(2);
-outparams.complex.R2 = pmin2(3);
-
-outparams.complex.fmin = fmin2;
-outparams.complex.chosenmin=2;
-
-% Calculate SSE for complex fitting
-[~,outparams.complex.SSE]=R2ComplexObj(pmin2,echotimes,tesla,Scomplex,sig);
-
-% Calculate SSE for complex fitting relative to noise-free signal
-[~,outparams.complex.SSEtrue]=R2ComplexObj(pmin2,echotimes,tesla,GT.S,sig);
-
-end
-
-% Calculate SSE for standard (Gaussian) for ground-truth initialised
-% parameters
-[~,outparams.complex.SSEgtinit]=R2ComplexObj(pmin3,echotimes,tesla,Scomplex,sig); %Relative to measured signal
-[~,outparams.complex.SSEtrue_gtinit]=R2ComplexObj(pmin3,echotimes,tesla,GT.S,sig); %Relative to ground-truth noise-free signal
+% Call ComplexFitting
+outparams.complexFixed = ComplexFitting (echotimes, tesla, Scomplex, sig, GT, algoparams);
 
 
 end
