db4ffe2524f55a48dddcb344351442408e65dcaf
TJPBray <t.bray@ucl.ac.uk>
2022-04-08 14:31:32 +0100

    SImplify to allow for agreement analysis in Hernando phantom datasets

--- Experiments/PhantomRoiAnalysis.m
+++ Experiments/PhantomRoiAnalysis.m
@@ -23,16 +23,16 @@ function PhantomRoiAnalysis(maps,phantomROIs,ReferenceValues,ffVendor,r2starVend
 %% 3. Reshape for visualisation
 
 %3.1 For FF measurements
-ff.standard.meangrid = reshape(ff.standard.mean,[4,5]);
-ff.rician.meangrid = reshape(ff.rician.mean,[4,5]);
-ff.complex.meangrid = reshape(ff.complex.mean,[4,5]);
-ff.vendor.meangrid = reshape(ff.vendor.mean,[4,5]);
-
-%3.2 For R2* measurements
-r2.standard.meangrid = reshape(r2.standard.mean,[4,5]);
-r2.rician.meangrid = reshape(r2.rician.mean,[4,5]);
-r2.complex.meangrid = reshape(r2.complex.mean,[4,5]);
-r2.vendor.meangrid = reshape(r2.vendor.mean,[4,5]);
+% ff.standard.meangrid = reshape(ff.standard.mean,[4,5]);
+% ff.rician.meangrid = reshape(ff.rician.mean,[4,5]);
+% ff.complex.meangrid = reshape(ff.complex.mean,[4,5]);
+% ff.vendor.meangrid = reshape(ff.vendor.mean,[4,5]);
+% 
+% %3.2 For R2* measurements
+% r2.standard.meangrid = reshape(r2.standard.mean,[4,5]);
+% r2.rician.meangrid = reshape(r2.rician.mean,[4,5]);
+% r2.complex.meangrid = reshape(r2.complex.mean,[4,5]);
+% r2.vendor.meangrid = reshape(r2.vendor.mean,[4,5]);
 
 %% 4. Display phantom images
 
@@ -52,7 +52,6 @@ colorbar
 subplot(1,4,3)
 imshow(maps.FFrician-maps.FFstandard,[-0.1 0.1])
 title('Fat fraction difference, Rician/MAGORINO - Gaussian/MAGO')
-
 
 subplot(1,4,3)
 imshow(maps.FFcomplex,[0 1])
@@ -93,173 +92,122 @@ colorbar
 % title('R2* complex MAGO-equivalent - Gaussian/MAGO')
 % colorbar
 
-%% 5. Display values as grids
+%% 5. Display agreement plots
 
 % 5.1 For FF
 figure('Name', 'FF')
+subplot(2,4,1)
+plot(ReferenceValues.FF,ff.standard.mean,'x-','LineWidth',1)
+hold on
+plot(ReferenceValues.FF,ff.rician.mean,'x--','LineWidth',1)
+plot(ReferenceValues.FF,0.01*ff.vendor.mean,'x--','LineWidth',1)
+xlabel('Reference FF')
+ylabel('PDFF')
+legend('Gaussian fitting','Rician fitting','Hernando complex fitting')
+xlim([0 1])
+ylim([0 1])
 
-s1=subplot(2,4,1)
-image(ReferenceValues.FF,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 1];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('Reference FF')
-colorbar
+%% 5. Display values as grids
 
-s1=subplot(2,4,2)
-image(ff.standard.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 1];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('Gaussian FF')
-colorbar
-
-s1=subplot(2,4,3)
-image(ff.rician.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 1];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('Rician FF')
-colorbar
-
-s1=subplot(2,4,4)
-image(ff.complex.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 1];
-xticks([1 2 3 4 5 6 7 8 9 10 11]);
-xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
-xlabel('R2* (ms^-^1)','FontSize',12)
-yticks([1 6 11 16 21 26 31 36 41 46 51]);
-yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
-ylabel('Fat fraction (%)','FontSize',12)
-title('Complex FF')
-colorbar
-
-
-s1=subplot(2,4,6)
-image(ff.standard.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[-1 1];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('Gaussian FF error')
-colorbar
-
-s1=subplot(2,4,7)
-image(ff.rician.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[-1 1];
-xticks([1 2 3 4 5 6 7 8 9 10 11]);
-xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
-xlabel('R2* (ms^-^1)','FontSize',12)
-yticks([1 6 11 16 21 26 31 36 41 46 51]);
-yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
-ylabel('Fat fraction (%)','FontSize',12)
-title('Rician FF error')
-colorbar
-
-s1=subplot(2,4,8)
-image(ff.complex.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[-1 1];
-xticks([1 2 3 4 5 6 7 8 9 10 11]);
-xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
-xlabel('R2* (ms^-^1)','FontSize',12)
-yticks([1 6 11 16 21 26 31 36 41 46 51]);
-yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
-ylabel('Fat fraction (%)','FontSize',12)
-title('Complex FF error')
-colorbar
-
-% 5.1 For R2star
-
-figure('Name', 'R2*')
-s1=subplot(2,3,1)
-image(r2.standard.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 0.3];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('R2* Gaussian/MAGO')
-colorbar
-
-s1=subplot(2,3,2)
-image(r2.rician.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 0.3];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('R2* Rician/MAGORINO')
-colorbar
-
-s1=subplot(2,3,3)
-image(r2.complex.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[0 0.3];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('R2* complex MAGO-equivalent')
-colorbar
-
-s1=subplot(2,3,5)
-image(r2.rician.meangrid-r2.standard.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[-0.03 0.03];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('R2* Rician/MAGORINO - Gaussian/MAGO')
-colorbar
-
-s1=subplot(2,3,6)
-image(r2.complex.meangrid-r2.standard.meangrid,'CDataMapping','scaled')
-ax=gca;
-ax.CLim=[-0.03 0.03];
-xticks([1 2 3 4 5]);
-xticklabels({'0','20', '40', '50', '60'});
-xlabel('Fat fraction (%)','FontSize',12)
-yticks([1 2 3 4]);
-yticklabels({'150','100','50','0'});
-ylabel('Bone mineral density','FontSize',12)
-title('R2* complex MAGO-equivalent - Gaussian/MAGO')
-colorbar
-
-% s1=subplot(1,4,3)
-% image(r2.vendor.meangrid,'CDataMapping','scaled')
+% % 5.1 For FF
+% figure('Name', 'FF')
+% 
+% s1=subplot(2,4,1)
+% image(ReferenceValues.FF,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[0 1];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('Reference FF')
+% colorbar
+% 
+% s1=subplot(2,4,2)
+% image(ff.standard.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[0 1];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('Gaussian FF')
+% colorbar
+% 
+% s1=subplot(2,4,3)
+% image(ff.rician.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[0 1];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('Rician FF')
+% colorbar
+% 
+% s1=subplot(2,4,4)
+% image(ff.complex.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[0 1];
+% xticks([1 2 3 4 5 6 7 8 9 10 11]);
+% xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
+% xlabel('R2* (ms^-^1)','FontSize',12)
+% yticks([1 6 11 16 21 26 31 36 41 46 51]);
+% yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
+% ylabel('Fat fraction (%)','FontSize',12)
+% title('Complex FF')
+% colorbar
+% 
+% s1=subplot(2,4,6)
+% image(ff.standard.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[-1 1];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('Gaussian FF error')
+% colorbar
+% 
+% s1=subplot(2,4,7)
+% image(ff.rician.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[-1 1];
+% xticks([1 2 3 4 5 6 7 8 9 10 11]);
+% xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
+% xlabel('R2* (ms^-^1)','FontSize',12)
+% yticks([1 6 11 16 21 26 31 36 41 46 51]);
+% yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
+% ylabel('Fat fraction (%)','FontSize',12)
+% title('Rician FF error')
+% colorbar
+% 
+% s1=subplot(2,4,8)
+% image(ff.complex.meangrid-ReferenceValues.FF,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[-1 1];
+% xticks([1 2 3 4 5 6 7 8 9 10 11]);
+% xticklabels({'0','.1', '.2', '.3', '.4', '.5', '.6', '.7', '.8', '.9','1.0'});
+% xlabel('R2* (ms^-^1)','FontSize',12)
+% yticks([1 6 11 16 21 26 31 36 41 46 51]);
+% yticklabels({'0','10','20','30','40','50','60','70','80','90','100'});
+% ylabel('Fat fraction (%)','FontSize',12)
+% title('Complex FF error')
+% colorbar
+% 
+% % 5.1 For R2star
+% 
+% figure('Name', 'R2*')
+% s1=subplot(2,3,1)
+% image(r2.standard.meangrid,'CDataMapping','scaled')
 % ax=gca;
 % ax.CLim=[0 0.3];
 % xticks([1 2 3 4 5]);
@@ -268,22 +216,87 @@ colorbar
 % yticks([1 2 3 4]);
 % yticklabels({'150','100','50','0'});
 % ylabel('Bone mineral density','FontSize',12)
-% title('R2* vendor')
+% title('R2* Gaussian/MAGO')
 % colorbar
-
-% figure('Name', 'R2star')
-% s1=subplot(1,3,1)
-% image(ReferenceValues.R2,'CDataMapping','scaled')
+% 
+% s1=subplot(2,3,2)
+% image(r2.rician.meangrid,'CDataMapping','scaled')
 % ax=gca;
-% ax.CLim=[0 150];
+% ax.CLim=[0 0.3];
 % xticks([1 2 3 4 5]);
 % xticklabels({'0','20', '40', '50', '60'});
 % xlabel('Fat fraction (%)','FontSize',12)
 % yticks([1 2 3 4]);
 % yticklabels({'150','100','50','0'});
 % ylabel('Bone mineral density','FontSize',12)
-% title('BMD')
+% title('R2* Rician/MAGORINO')
 % colorbar
+% 
+% s1=subplot(2,3,3)
+% image(r2.complex.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[0 0.3];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('R2* complex MAGO-equivalent')
+% colorbar
+% 
+% s1=subplot(2,3,5)
+% image(r2.rician.meangrid-r2.standard.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[-0.03 0.03];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('R2* Rician/MAGORINO - Gaussian/MAGO')
+% colorbar
+% 
+% s1=subplot(2,3,6)
+% image(r2.complex.meangrid-r2.standard.meangrid,'CDataMapping','scaled')
+% ax=gca;
+% ax.CLim=[-0.03 0.03];
+% xticks([1 2 3 4 5]);
+% xticklabels({'0','20', '40', '50', '60'});
+% xlabel('Fat fraction (%)','FontSize',12)
+% yticks([1 2 3 4]);
+% yticklabels({'150','100','50','0'});
+% ylabel('Bone mineral density','FontSize',12)
+% title('R2* complex MAGO-equivalent - Gaussian/MAGO')
+% colorbar
+% 
+% % s1=subplot(1,4,3)
+% % image(r2.vendor.meangrid,'CDataMapping','scaled')
+% % ax=gca;
+% % ax.CLim=[0 0.3];
+% % xticks([1 2 3 4 5]);
+% % xticklabels({'0','20', '40', '50', '60'});
+% % xlabel('Fat fraction (%)','FontSize',12)
+% % yticks([1 2 3 4]);
+% % yticklabels({'150','100','50','0'});
+% % ylabel('Bone mineral density','FontSize',12)
+% % title('R2* vendor')
+% % colorbar
+% 
+% % figure('Name', 'R2star')
+% % s1=subplot(1,3,1)
+% % image(ReferenceValues.R2,'CDataMapping','scaled')
+% % ax=gca;
+% % ax.CLim=[0 150];
+% % xticks([1 2 3 4 5]);
+% % xticklabels({'0','20', '40', '50', '60'});
+% % xlabel('Fat fraction (%)','FontSize',12)
+% % yticks([1 2 3 4]);
+% % yticklabels({'150','100','50','0'});
+% % ylabel('Bone mineral density','FontSize',12)
+% % title('BMD')
+% % colorbar
 
 end
 
