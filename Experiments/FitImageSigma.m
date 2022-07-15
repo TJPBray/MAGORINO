6c4736f967366c8b2d251e8bcaf77f386208c13d
TJPBray <t.bray@ucl.ac.uk>
2022-07-15 13:15:20 +0100

    Modify sigma estimation procedure to estimate from echo time closest to in phase

--- Experiments/FitImageSigma.m
+++ Experiments/FitImageSigma.m
@@ -1,6 +1,6 @@
 
-function [maps] = FitImageSigma(imData,slice,indent)
-% function [maps] = FitImage(imData,slice)
+function [maps,sigmaFromRoi,sigmaFromFit] = FitImageSigma(imData,roi)
+% function [maps,sigmaFromRoi,sigmaFromFit] = FitImage(imData,slice)
     
 %Takes data in imData structure and generates maps
 %Differs from FitImage in that sigma is estimated through fitting in each voxel prior
@@ -20,20 +20,28 @@ function [maps] = FitImageSigma(imData,slice,indent)
 %(matching the size of the images in imData.images)
 % This is applied to the specified slice to obtain sigma for Rician fitting)
 
-%4. Specify indent (integer value) to cut down processing time
+%4. fieldStrength is used to initialise sigma
+
+%5. Specify indent (integer value) to cut down processing time
 %e.g indent=100;
 
 %Outputs
 % Maps structure containing FF, R2* and sigma maps for Rician fitting
 
-%% 1. Reformat data where needed
 
-%Convert TE to ms
+%% 1. Reformat data where needed / preliminary steps 
+
+%1.1 Convert TE to ms
 TE=1000*imData.TE;
 
 TE=reshape(TE,6,1);
 
-%% 2. Get images for the chosen slice
+%1.2 Get indent
+indent = imData.fittingIndent;
+slice = roi.slice;
+
+
+%% 2. Get image data for the chosen slice
 for echoN=1:numel(TE)  
 Scomplex_slice(:,:,echoN)=imData.images(:,:,slice,1,echoN);
 end
@@ -54,37 +62,51 @@ s0Estimates=zeros(size(Smag_slice,1),size(Smag_slice,2));
 GT.p=[0 0 0 0];
 GT.S=[0 0 0 0 0 0];
 
-%% 4. Loop over voxels in image and fit each one 
+%% 4. Determine sigma value from raw signal intensities in the ROI (use the data from the echo time closest to in phase)
 
-for posX=(1+indent):(size(Smag_slice,2)-indent)     %Describes location of chosen pixel
-parfor posY=(1+indent):(size(Smag_slice,1)-indent)
+%4.1 Get sigma estimates for each TE
+
+for k=1:6
+Smag_slice_oneEcho = Smag_slice(:,:,k);
+maskedImageVals = Smag_slice_oneEcho(roi.mask==1); %For the multisite phantom data, this selects the pure water vial 
+stdMaskedIm(k) = std(maskedImageVals);
+end
+
+%4.2 Choose TE closest to in phase
+[~,TEindex] = min(abs(imData.TE - 0.0023));
+
+sigmaFromRoi = stdMaskedIm(TEindex);
+
+%% 5. Loop over voxels in image and fit each one 
+
+%5.1 Find indices of mask elements
+[y,x]=find(roi.mask==1);
+
+%5.2 Loop through each of the mask elements 
+for k = 1:numel(x) %numel x is number of pixels in mask
+
+    posX=x(k);
+    posY=y(k);
     
-%4.1 Get pixel data for single pixel
+    
+%5.4 Get pixel data for single pixel
 Smag=Smag_slice(posY,posX,:);
 Smag=reshape(Smag,1,6);
 
-%% 5. Implement fitting
+%5.5 Specify initialisation values and bounds 
+algoparams = setAlgoparams(Smag,sigmaFromRoi,2); %opt=2 specifies inclusion of bounds for sigma in algoParams
 
-%5.1 Determine sigma initialisation value depending on SNR
-if round(imData.FieldStrength,0)==3; %round field strength to nearest integer
-SNRest=60;
-elseif round(imData.FieldStrength,1)==1.5; %round field strength to nearest tenth
-SNRest=30;
-else error('Field Strength not recognised - sigma initialisation failed')
-end
+%% 6. Implement fitting
 
-%5.2 Specify initialisation values and bounds 
-algoparams = setAlgoparams(Smag,SNRest,2) %opt=2 specifies inclusion of bounds for sigma in algoParams
-
-%5.3 Check all signal values are nonzero (otherwise skip voxel) - Rician
+%6.1 Check all signal values are nonzero (otherwise skip voxel) - Rician
 %likelihood function returns an error otherwise
 
 if prod(Smag)>0
 
-%5.4 Run Rician fitting with sigma included as a parameter to estimate the value of sigma
+%6.2 Run Rician fitting with sigma included as a parameter to estimate the value of sigma
 outparams = RicianMagnitudeFitting_WithSigma(TE, imData.FieldStrength, Smag, algoparams.sigEst, GT, algoparams); %echotimes, fieldstrength, measured signal, initial estimate of sigma, ground truth initialisation, algoparams
 
-%5.5 Add values to parameter maps 
+%6.3 Add values to parameter maps 
 FFrician(posY,posX)=outparams.F/(outparams.F+outparams.W);
 % FFstandard(posY,posX)=outparams.standard.F/(outparams.standard.F+outparams.standard.W);
 % FFcomplex(posY,posX)=outparams.complex.F/(outparams.complex.F+outparams.complex.W);
@@ -100,12 +122,10 @@ s0Estimates(posY,posX)=outparams.F+outparams.W;
 else ;
 end
 
-
-posX
-
-end
+k/numel(x)
 
 end
+
 
 %Add maps to images structure
 maps.FFrician=FFrician;
@@ -132,11 +152,11 @@ colorbar
 title('R2*')
 
 subplot(1,3,3)
-imshow(maps.sigma,[0 100])
+imshow(maps.sigma,[])
 colorbar
 title('Sigma')
 
-
+sigmaFromFit = median(maps.sigma(maps.sigma>0));
 
 end
 
