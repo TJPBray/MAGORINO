024bf8a5154dc614ad9bfc31b4ef46aa4a46f313
TJPBray <t.bray@ucl.ac.uk>
2022-09-26 12:22:43 +0100

    Modification of image visualisations to remove mask and to show S0

--- Experiments/MultistepFitImage.m
+++ Experiments/MultistepFitImage.m
@@ -155,38 +155,82 @@ end
 maps = mapsFittedSigma; 
 
 %% 5. Show MAGORINO vs MAGO
+
+%Visualisation of distribution and mask derivation
 figure
-subplot(2,3,1)
-imshow(maps.FFstandard(indent:matSize-indent,indent:matSize-indent),[0 1])
+subplot(1,2,1)
+hist(reshape(maps.S0rician,1,[]),20)
+hold on
+%Get 95the percentile
+p=prctile(reshape(maps.S0rician,1,[]),50)
+
+plot([p p],ylim)
+hold off
+
+%Calculate mask
+mask = maps.S0rician>p;
+
+mask = double(mask);
+
+%Create NaNs for zero values
+mask(mask==0) = NaN;
+
+%Show mask
+subplot(1,2,2)
+imshow(mask,[])
+
+figure
+subplot(3,3,1)
+imshow(maps.FFstandard,[0 1])
 title('PDFF Gaussian')
 colormap('parula')
 colorbar
 
-subplot(2,3,2)
-imshow(maps.FFrician(indent:matSize-indent,indent:matSize-indent),[0 1])
+subplot(3,3,2)
+imshow(maps.FFrician,[0 1])
 title('PDFF Rician')
 colormap('parula')
 colorbar
 
-subplot(2,3,3)
-imshow(maps.FFrician(indent:matSize-indent,indent:matSize-indent) - maps.FFstandard(indent:matSize-indent,indent:matSize-indent),[-0.1 0.1])
+subplot(3,3,3)
+imshow((maps.FFrician - maps.FFstandard),[-0.1 0.1])
 title('PDFF Rician - PDFF Gaussian')
 colorbar 
 
-subplot(2,3,4)
-imshow(1000*maps.R2standard(indent:matSize-indent,indent:matSize-indent),[0 250])
+subplot(3,3,4)
+imshow((1000*maps.R2standard),[0 250])
 title('R2* Gaussian')
 colorbar 
 
-subplot(2,3,5)
-imshow(1000*maps.R2rician(indent:matSize-indent,indent:matSize-indent),[0 250])
+subplot(3,3,5)
+imshow((1000*maps.R2rician),[0 250])
 title('R2* Rician')
 colorbar 
 
-subplot(2,3,6)
-imshow(1000*maps.R2rician(indent:matSize-indent,indent:matSize-indent) - 1000*maps.R2standard(indent:matSize-indent,indent:matSize-indent),[-50 50])
+subplot(3,3,6)
+imshow(mask.*(1000*maps.R2rician - 1000*maps.R2standard),[-50 50])
 title('R2* Rician - R2* Gaussian')
 colorbar 
+
+subplot(3,3,7)
+imshow(maps.S0standard,[0 max(maps.S0standard,[],'all')])
+title('S0 Gaussian')
+colorbar 
+
+subplot(3,3,8)
+imshow(maps.S0rician,[0 max(maps.S0standard,[],'all')])
+title('S0 Rician')
+colorbar 
+
+subplot(3,3,9)
+imshow((maps.S0rician - maps.S0standard),[-0.01*max(maps.S0standard,[],'all') 0.01*max(maps.S0standard,[],'all')])
+title('S0 Rician - S0 Gaussian')
+colorbar 
+
+
+
+
+
 
 
 
