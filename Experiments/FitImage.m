8790a70d03e6d07b48da369e70a9498af0b78ed9
TJPBray <t.bray@ucl.ac.uk>
2022-09-26 12:23:00 +0100

    Addition of S0 to fitimage

--- Experiments/FitImage.m
+++ Experiments/FitImage.m
@@ -69,6 +69,10 @@ FFcomplex=zeros(size(Smag_slice,1),size(Smag_slice,2));
 R2rician=zeros(size(Smag_slice,1),size(Smag_slice,2));
 R2standard=zeros(size(Smag_slice,1),size(Smag_slice,2));
 R2complex=zeros(size(Smag_slice,1),size(Smag_slice,2));
+S0rician=zeros(size(Smag_slice,1),size(Smag_slice,2));
+S0standard=zeros(size(Smag_slice,1),size(Smag_slice,2));
+S0complex=zeros(size(Smag_slice,1),size(Smag_slice,2));
+
 
 %% Set ground truth values to 0
 GT.p=[0 0 0 0];
@@ -103,6 +107,10 @@ R2rician(posY,posX)=outparams.Rician.R2;
 R2standard(posY,posX)=outparams.standard.R2;
 R2complex(posY,posX)=outparams.complex.R2;
 
+S0rician(posY,posX)=outparams.Rician.F + outparams.Rician.W;
+S0standard(posY,posX)=outparams.standard.F + outparams.standard.W;
+R2complex(posY,posX)=outparams.complex.F + outparams.complex.W;
+
 posX
 
 else;
@@ -119,43 +127,58 @@ maps.FFcomplex=FFcomplex;
 maps.R2rician=R2rician;
 maps.R2standard=R2standard;
 maps.R2complex=R2complex;
+maps.S0rician=S0rician;
+maps.S0standard=S0standard;
+maps.S0complex=S0complex;
 
+%% Create figures
 figure
-subplot(2,3,1)
-imshow(maps.FFrician,[0 1])
-colorbar
-colormap('parula')
-title('PDFF rician')
-
-subplot(2,3,2)
-imshow(maps.R2rician,[0 0.3])
-colorbar
-colormap('gray')
-title('R2* rician')
-
-subplot(2,3,3)
-imshow(maps.FFrician - maps.FFstandard,[0 1])
-colorbar
-colormap('parula')
-title('PDFF rician')
-
-subplot(2,3,4)
+subplot(3,3,1)
 imshow(maps.FFstandard,[0 1])
-colorbar
+title('PDFF Gaussian')
 colormap('parula')
-title('PDFF gaussian')
-
-subplot(2,3,5)
-imshow(maps.R2standard,[0 0.3])
 colorbar
-colormap('gray')
-title('R2 gaussian')
 
-subplot(2,3,6)
-imshow(maps.R2rician - maps.R2standard,[-0.05 0.05])
+subplot(3,3,2)
+imshow(maps.FFrician,[0 1])
+title('PDFF Rician')
+colormap('parula')
 colorbar
-colormap('gray')
-title('R2% Rician - R2* Gaussian')
+
+subplot(3,3,3)
+imshow(maps.FFrician - maps.FFstandard,[-0.1 0.1])
+title('PDFF Rician - PDFF Gaussian')
+colorbar 
+
+subplot(3,3,4)
+imshow(1000*maps.R2standard,[0 250])
+title('R2* Gaussian')
+colorbar 
+
+subplot(3,3,5)
+imshow(1000*maps.R2rician,[0 250])
+title('R2* Rician')
+colorbar 
+
+subplot(3,3,6)
+imshow(1000*maps.R2rician - 1000*maps.R2standard,[-50 50])
+title('R2* Rician - R2* Gaussian')
+colorbar 
+
+subplot(3,3,7)
+imshow(maps.S0standard,[0 0.01*max(maps.S0standard,[],'all')])
+title('S0 Gaussian')
+colorbar 
+
+subplot(3,3,8)
+imshow(maps.S0rician,[0 0.01*max(maps.S0standard,[],'all')])
+title('S0 Rician')
+colorbar 
+
+subplot(3,3,9)
+imshow(maps.S0rician - maps.S0standard,[-0.01*max(maps.S0standard,[],'all') 0.01*max(maps.S0standard,[],'all')])
+title('S0 Rician - S0 Gaussian')
+colorbar 
 
 
 end
